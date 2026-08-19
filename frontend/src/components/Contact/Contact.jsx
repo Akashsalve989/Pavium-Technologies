c1eae56b659a9c1fa65aa2590312bf4b92966ae2
Akash Salve <akashsalve989@gmail.com>
2026-08-19 11:10:59 +0530

    Updated website

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ function Contact() {
             <FaEnvelope className="info-icon"/>
             <div>
               <h3>Email</h3>
-              <p>info@pavium.in</p>
+              <p>pavium.tech@gmail.com</p>
             </div>
           </div>
 
